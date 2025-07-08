ba7591ddff5425b76fd69d7b95875d1327f7b1e5
Ardinata <angga.ardinata@tekuton.id>
2025-07-09 05:32:21 +0700

    Grouping Mobile

---
+++ features/feature_util/src/commonMain/kotlin/app/tktn/feature_util/PlatformUtil.kt
@@ -0,0 +1,5 @@
+package app.tktn.feature_util
+
+object PlatformUtil
+
+expect fun PlatformUtil.toast(message: String)
---
+++ features/feature_util/src/mobileMain/kotlin/app/tktn/feature_util/PlatformUtil.mobile.kt
@@ -0,0 +1,5 @@
+package app.tktn.feature_util
+
+actual fun PlatformUtil.toast(message: String) {
+
+}
